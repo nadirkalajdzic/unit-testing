09cd80ae973b851476a528dbf2178164031bf6fb
Nadir Kalajdzic <nadirka@maestralsolutions.com>
2021-08-27 16:19:48 +0200

    init commit for unit testing project

---
+++ Sparky/SparkyMSTest/CalculatorMSTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    class CalculatorMSTest
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // arrange
+            Calculator calc = new();
+
+            // act
+            int result = calc.AddNumbers(10, 20);
+
+            // assert
+            Assert.AreEqual(30, result);
+
+        }
+    }
+
+}
---
+++ Sparky/SparkyMSTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
