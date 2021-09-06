85acab01112d94a728c7cf1c7cc00280d3593348
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2021-09-06 12:12:58 +0200

    added logbook tests

---
+++ Sparky/Sparky/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class BankAccount
+    {
+        public int balance { get; set; }
+        private readonly ILogBook _logBook; 
+        public BankAccount(ILogBook logBook)
+        {
+            _logBook = logBook;
+            balance = 0;
+        }
+
+        public bool Deposit(int amount)
+        {
+            _logBook.Message("Deposit invoked");
+            balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if(amount <= balance)
+            {
+                balance -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+        [SetUp]
+        public void SetUp()
+        {
+            bankAccount = new(new LogBook());
+        }
+
+        [Test]
+        public void BankDeposit_Add100_ReturnTrue()
+        {
+            var result = bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        }
+
+    }
+}
