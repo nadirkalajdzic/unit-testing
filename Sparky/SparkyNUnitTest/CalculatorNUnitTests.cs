058454b82cb4e2048b22cddc618a256cacd1ac65
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2021-09-06 09:13:05 +0200

    added nunit tests

---
+++ Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // arrange
+            Calculator calc = new();
+
+            // act
+            int result = calc.AddNumbers(10, 20);
+
+            // assert
+            Assert.AreEqual(30, result);
+
+        }
+    }
+}
