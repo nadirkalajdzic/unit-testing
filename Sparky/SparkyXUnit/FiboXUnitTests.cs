adeaaa9a71089a3f205ab4416020291f2f6cf1e9
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2021-09-06 13:00:51 +0200

    assignment 4 and 5 done

---
+++ Sparky/SparkyXUnit/FiboXUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Sparky
+{
+    public class FiboXUnitTests
+    {
+        [Fact]
+        public void FiboChecker_Input1_ReturnFiboSeries()
+        {
+            List<int> expectedRange = new() { 0 };
+
+            Fibo fibo = new();
+            fibo.Range = 1;
+
+            List<int> result = fibo.GetFiboSeries();
+
+            Assert.NotEmpty(result);
+            Assert.Equal(expectedRange.OrderBy(x => x), result);
+            Assert.True(result.SequenceEqual(expectedRange));
+        }
+
+        [Fact]
+        public void FiboChecker_Input6_ReturnFiboSeries()
+        {
+            List<int> expectedRange = new() { 0, 1, 1, 2, 3, 5 };
+
+            Fibo fibo = new();
+            fibo.Range = 6;
+
+            List<int> result = fibo.GetFiboSeries();
+
+            Assert.Contains(3, result);
+            Assert.Equal(6, result.Count);
+            Assert.DoesNotContain(4 ,result);
+            Assert.Equal(expectedRange, result);
+        }
+    }
+}
